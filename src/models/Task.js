af4dbcecb0c7cf23f8b227be4f36e5d8255d3a8c
Mendes Eduardo <eduardomn2018@gmail.com>
2023-02-18 18:34:33 -0300

    add completed

--- src/models/Task.js
+++ src/models/Task.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
     id: { type: String },
     title: { type: String, required: true },
-    completed: { type: Boolean }
+    completed: { type: Boolean, default: false }
 });
 
 const tasks = mongoose.model('Task', taskSchema);
